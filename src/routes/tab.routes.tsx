c15c3981164c565598b6a0f369fc8e3fd454dcfa
Marcus Vinicius Koch <hagnakoch@gmail.com>
2021-12-16 07:57:17 -0300

    alegria

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 // Screens
-import Home from '../screens/home'
+import Home from '../screens/Home'
 
 // Themes
 import themes from '../styles/index'
